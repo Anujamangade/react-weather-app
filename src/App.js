6b3f89cf973e24a8f7c314d258e96a78937deb00
Anujamangade <155188610+Anujamangade@users.noreply.github.com>
2025-07-06 19:56:09 +0530

    WA-8 comment addressed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save and run.
+          Edit <code>src/App.js</code> and run and save.
         </p>
         <a
           className="App-link"
